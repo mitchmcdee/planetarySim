b4e70e47fda3df26da7229ca343baa2027385ccf
Mitchell McDermott <mitchmcdee@gmail.com>
2016-05-03 19:44:33 +1000

    Patched background sphere graphical bug by adjusting bit depth comparitors. Performed refactoring of code.

--- planetSim.py
+++ planetSim.py
@@ -10,56 +10,23 @@ import random
 import os
 os.chdir('/Users/mcdee/Uni/COSC3000/Planet Simulator/')
 
+#Global variables
+speedZoomToggle = 0
 rotateToggle = 0
 keyDown = []
 
-def OldInput():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_move = direction_speed
-            if event.key == pygame.K_RIGHT:
-                x_move = -1*direction_speed
-
-            if event.key == pygame.K_UP:
-                y_move = -1*direction_speed
-            if event.key == pygame.K_DOWN:
-                y_move = direction_speed
-
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_move = 0
-
-            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                y_move = 0
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                rotateToggle = 1
-
-            if event.button == 4:
-                #glTranslatef(0,0,10.0)
-                glScalef(1.1,1.1,1.1)
-
-            if event.button == 5:
-                #glTranslatef(0,0,-10.0)
-                glScalef(0.9,0.9,0.9)
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 4:
-                glTranslatef(0,0,1.0)
-
-            if event.button == 5:
-                glTranslatef(0,0,-1.0)
+#Global constants
+BACKGROUND_SPHERE_RADIUS = 100000
+NUM_BODIES = 200
+MAX_BODY_RADIUS = 300
+NEAR_CLIP_DISTANCE = 0.01
+FAR_CLIP_DISTANCE = 1000000
+RENDER_DISTANCE = 10000
+FIELD_OF_VIEW = 45
+NUM_RENDER_LINES = 80
 
 def NewInput():
-    global rotateToggle
-    global keyDown
+    global rotateToggle, keyDown, speedZoomToggle
 
     for event in pygame.event.get():
         print(event)
@@ -69,10 +36,16 @@ def NewInput():
             quit()
 
         if event.type == pygame.KEYDOWN:
-            keyDown.append(event.key)
+            if event.key == pygame.K_LSHIFT:
+                speedZoomToggle = 1
+            else:
+                keyDown.append(event.key)
 
         if event.type == pygame.KEYUP:
-            keyDown.remove(event.key)
+            if event.key == pygame.K_LSHIFT:
+                speedZoomToggle = 0
+            else:
+                keyDown.remove(event.key)
 
         if event.type == pygame.MOUSEMOTION and rotateToggle:
             relx, rely = event.rel
@@ -91,14 +64,20 @@ def NewInput():
                 rotateToggle = 1
 
             if event.button == 4:
-                glTranslatef(0,0,20.0)
+                if speedZoomToggle:
+                    glTranslatef(0,0,200.0)
+                else:
+                    glTranslatef(0,0,20.0)
                 #glPushMatrix()
                 #glLoadIdentity()
                 #glScalef(1.1,1.1,1.1)
                 #glPopMatrix()
 
             if event.button == 5:
-                glTranslatef(0,0,-20.0)
+                if speedZoomToggle:
+                    glTranslatef(0,0,-200.0)
+                else:
+                    glTranslatef(0,0,-20.0)
                 #glScalef(0.9,0.9,0.9)
 
     #move out into action method?
@@ -114,7 +93,7 @@ def NewInput():
             glTranslatef(0,-20,0)
         
 
-def TexFromPNG(filename):
+def TextureFromImage(filename):
     img = Image.open(filename)
     img_data = numpy.array(list(img.getdata()), numpy.uint8)
 
@@ -131,43 +110,49 @@ def TexFromPNG(filename):
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.size[0], img.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
     return texture
 
-class Sphere(object):
-    def __init__(self, placement):
-        """Draws a centered sphere with radius s in given color."""
+def Sphere(placement):
+    global NUM_RENDER_LINES
+    """Draws a centered sphere with radius s in given color."""
 
-        self.x_pos = placement[0]
-        self.y_pos = placement[1]
-        self.z_pos = placement[2]
+    x_pos = placement[0]
+    y_pos = placement[1]
+    z_pos = placement[2]
+    texture = placement[3]
+    radius = placement[4]
 
-        glTranslatef(self.x_pos, self.y_pos, self.z_pos)
+    glTranslatef(x_pos, y_pos, z_pos)
 
-        quad = gluNewQuadric()
-        gluQuadricNormals(quad, GLU_SMOOTH)
-        gluQuadricTexture(quad,GL_TRUE)
-        glEnable(GL_TEXTURE_2D)
-        glBindTexture(GL_TEXTURE_2D, placement[3])
-        gluSphere(quad,placement[4],100,100)
-        glDisable(GL_TEXTURE_2D)
+    quad = gluNewQuadric()
+    gluQuadricNormals(quad, GLU_SMOOTH)
+    gluQuadricTexture(quad, GL_TRUE)
+    glEnable(GL_TEXTURE_2D)
+    glBindTexture(GL_TEXTURE_2D, texture)
+    gluSphere(quad, radius, NUM_RENDER_LINES, NUM_RENDER_LINES)
+    glDisable(GL_TEXTURE_2D)
 
-        glTranslatef(-self.x_pos, -self.y_pos, -self.z_pos)
+    glTranslatef(-x_pos, -y_pos, -z_pos)
+
+def biasRandom(massDist):
+    randRoll = random.random() # in [0,1)
+    sum = 0
+    result = 1
+    for mass in massDist:
+        sum += mass
+        if randRoll < sum:
+            return result
+        result+=1
+
+#TODO(mitch): Add in ability to have absolute placement?
+def set_placement(texture, render_distance, absolute = False):
+    global MAX_BODY_RADIUS
+    new_placement = []
     
-    def getKey(self):
-        return self.z_pos
-
-
-
-def set_placement(texture, MAX_RENDER_DISTANCE, min_distance = -20, camera_x = 0, camera_y = 0):
-
-    camera_x = -1*int(camera_x)
-    camera_y = -1*int(camera_y)
-
-    new_placement = [0,0,0,0,0]
-    
-    new_placement[0] = random.randrange(camera_x-800,camera_x+800)
-    new_placement[1] = random.randrange(camera_y-800,camera_y+800)
-    new_placement[2] = random.randrange(-1*MAX_RENDER_DISTANCE,min_distance)
-    new_placement[3] = texture
-    new_placement[4] = random.randrange(0,30)
+    new_placement.append(random.randrange(-(render_distance / 2),(render_distance / 2)))
+    new_placement.append(random.randrange(-(render_distance / 2),(render_distance / 2)))
+    new_placement.append(random.randrange(-(render_distance / 2),(render_distance / 2)))
+    new_placement.append(texture)
+    #inverse transform sampling to the 10th root.
+    new_placement.append(MAX_BODY_RADIUS * (1 - (1 - random.random())**(1.0/10.0)))
 
     return new_placement
 
@@ -202,85 +187,71 @@ def resetDisplay(texture):
 
 
 def main():
+    global RENDER_DISTANCE, FIELD_OF_VIEW, NEAR_CLIP_DISTANCE, FAR_CLIP_DISTANCE, \
+            NUM_BODIES, BACKGROUND_SPHERE_RADIUS
+
+    #TODO(mitch): make init method?
+
     pygame.init()
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
     
     glEnable(GL_DEPTH_TEST)
-    glDepthFunc(GL_LEQUAL) #These add weird clipping, was GL_LESS
-
-    NEAR_CLIP_DISTANCE = 0.01
-    FAR_CLIP_DISTANCE = 1000000
-    MAX_RENDER_DISTANCE = 1600
-    FIELD_OF_VIEW = 60
+    glDepthFunc(GL_ALWAYS)
     
     glMatrixMode(GL_PROJECTION)
     gluPerspective(FIELD_OF_VIEW, (display[0]/display[1]), NEAR_CLIP_DISTANCE, FAR_CLIP_DISTANCE)
-    glTranslatef(0,0, -40)
-
-    x_move = 0
-    y_move = 0
-
-    cur_x = 0
-    cur_y = 0
+    
+    #Starting view position
+    #glTranslatef(0,0, RENDER_DISTANCE / 2)
 
     game_speed = 0.1
-    direction_speed = 2
-
-    background = TexFromPNG(os.path.join("images","bg.png"))
-
-    bodies = []
-    bodies.append(TexFromPNG(os.path.join("images","planet1.png")))
-    bodies.append(TexFromPNG(os.path.join("images","planet2.png")))
-    bodies.append(TexFromPNG(os.path.join("images","planet3.png")))
-    bodies.append(TexFromPNG(os.path.join("images","planet4.png")))
-    bodies.append(TexFromPNG(os.path.join("images","planet5.png")))
-    bodies.append(TexFromPNG(os.path.join("images","planet6.png")))
-    bodies.append(TexFromPNG(os.path.join("images","planet7.png")))
-    bodies.append(TexFromPNG(os.path.join("images","planet8.png")))
-    bodies.append(TexFromPNG(os.path.join("images","planet9.png")))
-
+    x_move = 0
+    y_move = 0
     sphere_list = []
+    bodies = []
 
-    for x in range(100):
-        sphere_list.append(set_placement(bodies[random.randrange(0,len(bodies))],MAX_RENDER_DISTANCE))
+    background = TextureFromImage(os.path.join("images","bg.png"))
+
+    bodies.append(TextureFromImage(os.path.join("images","planet1.png")))
+    bodies.append(TextureFromImage(os.path.join("images","planet2.png")))
+    bodies.append(TextureFromImage(os.path.join("images","planet3.png")))
+    bodies.append(TextureFromImage(os.path.join("images","planet4.png")))
+    bodies.append(TextureFromImage(os.path.join("images","planet5.png")))
+    bodies.append(TextureFromImage(os.path.join("images","planet6.png")))
+    bodies.append(TextureFromImage(os.path.join("images","planet7.png")))
+    bodies.append(TextureFromImage(os.path.join("images","planet8.png")))
+    bodies.append(TextureFromImage(os.path.join("images","planet9.png")))
+
+
+    #generate spheres TODO(mitch): add in random chance for moon, add in clusters?
+    for x in range(NUM_BODIES):
+        sphere_list.append(set_placement(bodies[random.randrange(0,len(bodies))], RENDER_DISTANCE))
     
+    #Sort spheres in order of their z-distance TODO(mitch): change to distance from cam?
     sphere_list.sort(key=lambda x: x[2])
 
     while True:
         #resetDisplay(background)
 
-        glClearColor (0.0,0.0,0.0,1.0)
-        glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT )
+        #Clear display
+        glClearColor(0.0,0.0,0.0,1.0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT )
         
+        #Get and react to user input
         NewInput()
-                
-        x = glGetDoublev(GL_MODELVIEW_MATRIX)
-        
-        camera_x = x[3][0]
-        camera_y = x[3][1]
-        camera_z = x[3][2]
 
-        
-        cur_x += x_move
-        cur_y += y_move
+        #Draw background
+        Sphere([0, 0, 0, background, BACKGROUND_SPHERE_RADIUS])
 
-        Sphere([0,0,0,background,100000])
-
+        #Move spheres
         glTranslatef(x_move,y_move,game_speed)
 
-
+        #Draw all spheres
         for each_sphere in sphere_list:
             Sphere(each_sphere)
 
-        for i,each_sphere in enumerate(sphere_list):
-            if camera_z <= each_sphere[2]:
-                new_max = int(-1*(camera_z-(MAX_RENDER_DISTANCE*2)))
-                #sphere_list.append(set_placement(bodies[random.randrange(0,len(bodies))],new_max,int(camera_z-MAX_RENDER_DISTANCE), cur_x, cur_y))
-                sphere_list[i] = set_placement(bodies[random.randrange(0,len(bodies))],new_max,int(camera_z-MAX_RENDER_DISTANCE), cur_x, cur_y)   
-        
-        sphere_list.sort(key=lambda x: x[2])
-
+        #Flip display buffer
         pygame.display.flip()
 
 
